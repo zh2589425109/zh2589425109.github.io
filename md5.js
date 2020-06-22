f1ec1c8dfe6d1f68d86ace98e18f690d8b7118c0
438762904@qq.com <2589425109@qq.com>
2020-06-22 09:35:33 +0800

    Update md5.js

--- md5.js
+++ md5.js
@@ -1,3 +1,25 @@
 {
-    "keyLength": 64
-}
+    "keyLength": 2048,
+    "io": false,
+    "webUrl": "https://update-0001.17itou.com/hot_code_com.zhanzhuyun.kdkb_adhoc/koudai/",
+    "thirdPartUrlPrefix": [
+      "https://cash.yeepay.com",
+      "https://m.51yueqian.com",
+      "https://wap.beeplaying.com",
+      "http://cash.yeepay.com",
+      "http://m.51yueqian.com",
+      "http://wap.beeplaying.com"
+    ],
+    "schemeLaunchUrl": [
+      "weixin://",
+      "sms://",
+      "tel://",
+      "alipays://",
+      "mailto:"
+    ],
+    "syncCookiesUrl": "https://www.17itou.com",
+    "hotCodeUrl": "https://update-0001.17itou.com/hot_code_com.zhanzhuyun.kdkb_adhoc/koudai/",
+    "hotCodeZipUrl": "https://update-0001.17itou.com/hot_code_com.zhanzhuyun.kdkb_adhoc/koudai_zip/",
+    "enableHotCode": true,
+    "enableHotCodeZip": true
+  }
