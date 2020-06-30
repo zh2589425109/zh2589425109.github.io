ac5245b11f3b7516ed1922b5a08f07e92e69edb1
zh2589425109 <2589425109@qq.com>
2020-06-30 10:14:49 +0800

    Update md5.js

--- md5.js
+++ md5.js
@@ -1,26 +1,28 @@
 {
-    "keyLength": 2048,
-    "io": false,
-    "webUrl": "https://update-0001.17itou.com/hot_code_com.zhanzhuyun.kdkb_dist/koudai/",
-    "thirdPartUrlPrefix": [
-      "https://cash.yeepay.com",
-      "https://m.51yueqian.com",
-      "https://wap.beeplaying.com",
-      "http://cash.yeepay.com",
-      "http://m.51yueqian.com",
-      "http://wap.beeplaying.com"
-    ],
-    "schemeLaunchUrl": [
-      "weixin://",
-      "sms://",
-      "tel://",
-      "alipays://",
-      "mailto:"
-    ],
-    "syncCookiesUrl": "https://www.17itou.com",
-    "hotCodeUrl": "https://update-0001.17itou.com/hot_code_com.zhanzhuyun.kdkb_dist/koudai/",
-    "hotCodeZipUrl": "https://update-0001.17itou.com/hot_code_com.zhanzhuyun.kdkb_dist/koudai_zip/",
-    "enableHotCode": true,
-    "enableHotCodeZip": true,
-    "enableBlue": false
-  }
+  "keyLength": 2048,
+  "io": false,
+  "webUrl": "https://update-0001.17itou.com/hot_code_com.zhanzhuyun.kdkb_dist/koudai/",
+  "thirdPartUrlPrefix": [
+    "https://cash.yeepay.com",
+    "https://m.51yueqian.com",
+    "https://wap.beeplaying.com",
+    "https://h5.jdddata.com/communal/cartoonKou/fulllive",
+    "http://cash.yeepay.com",
+    "http://m.51yueqian.com",
+    "http://wap.beeplaying.com",
+    "http://h5.jdddata.com/communal/cartoonKou/fulllive"
+  ],
+  "schemeLaunchUrl": [
+    "weixin://",
+    "sms://",
+    "tel://",
+    "alipays://",
+    "mailto:"
+  ],
+  "syncCookiesUrl": "https://www.17itou.com",
+  "hotCodeUrl": "https://update-0001.17itou.com/hot_code_com.zhanzhuyun.kdkb_dist/koudai/",
+  "hotCodeZipUrl": "https://update-0001.17itou.com/hot_code_com.zhanzhuyun.kdkb_dist/koudai_zip/",
+  "enableHotCode": true,
+  "enableHotCodeZip": true,
+  "enableBlue": false
+}
